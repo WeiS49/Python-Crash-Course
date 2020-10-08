dd059fcb2f354e0266041ca8690926ad738d770a
WeiS49 <swh_1C3@outlook.com>
2020-10-08 20:27:02 +0800

    ch3 list

---
+++ python_work/ch3/3.1 bicycles.py
@@ -0,0 +1,9 @@
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+print(bicycles[0].title())
+print(bicycles[-2].title())
+
+message = f'My first bicycle was a {bicycles[0].title()}'
+print(message)
+
