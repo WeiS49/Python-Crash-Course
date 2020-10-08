649c690869099937612f70e9951d45d74d80dee1
WeiS49 <swh_1C3@outlook.com>
2020-10-08 20:05:44 +0800

    ch2 practice2

---
+++ python_work/ch2/practice/2-8 number 8.py
@@ -0,0 +1,11 @@
+
+expression1 = 5 + 3
+expression2 = 10 - 2
+expression3 = 2 * 4
+expression4 = 48 // 6
+
+print(expression1, expression2, expression3, expression4)
+
+
+
+
---
+++ python_work/ch2/practice/2-9 favorite number.py
@@ -0,0 +1,4 @@
+
+favorite_number = 451
+
+print(f"My favorite number is {favorite_number}.")
