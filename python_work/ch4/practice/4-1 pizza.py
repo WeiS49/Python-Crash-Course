4ffb421f98d77aa7143d74a85158531a03f770ef
WeiS49 <swh_1C3@outlook.com>
2020-10-10 20:01:14 +0800

    ch4 practice1

---
+++ python_work/ch4/practice/4-1 pizza.py
@@ -0,0 +1,8 @@
+
+lots_of_pizza = ['pizza1', 'pizza2', 'pizza3']
+
+for pizza in lots_of_pizza:
+    print(f"I like {pizza}")
+
+print("I really love pizza!")
+
---
+++ python_work/ch4/practice/4-2 animal.py
@@ -0,0 +1,10 @@
+
+four_feet_animal = ['horse', 'donkey', 'sheep']
+
+for animal in four_feet_animal:
+    print(f"A {animal} would be a great pet.\n")
+
+print("Any of these animals would make a great pet!")
+
+
+
