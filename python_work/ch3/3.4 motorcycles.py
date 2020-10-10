de483de412f640d2f52533f620efd3aac44572f5
WeiS49 <swh_1C3@outlook.com>
2020-10-10 18:22:45 +0800

    ch3 practice & 3.4

---
+++ python_work/ch3/3.4 motorcycles.py
@@ -0,0 +1,10 @@
+
+motorcycles = ['hodna', 'yamaha', 'suzuki']
+
+print(motorcycles[3]) # 超出列表长度 错误
+
+print(motorcycles[-1]) # 不论列表长度, 返回最后一个元素 
+
+listx = []
+
+print(listx[-1]) # 当列表为空时, 返回最后一个元素时会报错
---
+++ python_work/ch3/practice/3-8 travel.py
@@ -0,0 +1,20 @@
+
+travel_destination = ['Paris', 'Tokyo', 'New York', 'Washington', 'Peking']
+
+print(sorted(travel_destination))
+
+print(travel_destination)
+
+print(sorted(travel_destination, reverse=True))
+
+print(travel_destination)
+
+travel_destination.sort()
+
+print(travel_destination)
+
+print(travel_destination)
+
+del travel_destination  # 这条语句能直接删除变量
+
+# print(travel_destination)
