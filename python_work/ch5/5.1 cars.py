670173cbfbde587dac6252ba755a8ec6ec70d867
WeiS49 <swh_1C3@outlook.com>
2020-10-13 17:11:06 +0800

    ch5 5.1 & 5.2

---
+++ python_work/ch5/5.1 cars.py
@@ -0,0 +1,14 @@
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())  # 该方法只临时起作用
+
+    else:
+        print(car.title())
+
+
+# '==' 判断 区分大小写
+
+
---
+++ python_work/ch5/5.2 toppings.py
@@ -0,0 +1,14 @@
+
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+
+if 'mushrooms' in requested_toppings:   # 判断某个元素是否在该数据(列表 字典 字符串)中
+    print("The pizza you ordered has mushrooms.")
+
+dic1 = {'key1':'value1', 'key2':'value2', 'key3':'value3'}  # 字典默认遍历key值
+
