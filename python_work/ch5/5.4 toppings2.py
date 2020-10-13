37b2cd3dfccccf99a3030247a03d714ad9624265
WeiS49 <swh_1C3@outlook.com>
2020-10-13 18:41:50 +0800

    ch5 5.4

---
+++ python_work/ch5/5.4 toppings2.py
@@ -0,0 +1,10 @@
+
+requested_toppings2 = ['123']
+
+if requested_toppings2:
+    for requested_topping in requested_toppings2:
+        print(f"Adding {requested_topping}")
+
+else:
+    print("\na plain pizza")        
+
