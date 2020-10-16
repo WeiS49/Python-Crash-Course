fa0dc86a9102e5c4f192171e483a6af3e15b67f1
WeiS49 <swh_1C3@outlook.com>
2020-10-16 12:08:42 +0800

    ch8 practice

---
+++ python_work/ch8/practice/8-1 message.py
@@ -0,0 +1,9 @@
+
+def display_message():
+    """ Print what I learned in this chapter. """
+    print("I am study ch8 -- How to use function in Python")
+
+display_message()
+
+
+
---
+++ python_work/ch8/practice/8-2 favorite_books.py
@@ -0,0 +1,12 @@
+
+def favorite_book(title):
+    """ Print my favorite book in English. """
+    print(f"One of my favorite books is {title.title()}")
+
+book = 'alice in wonderland'
+favorite_book(book)
+
+
+
+
+
