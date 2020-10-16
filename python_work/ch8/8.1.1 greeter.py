c916caf4b6e68524cb560a18c1c578554a5fac80
WeiS49 <swh_1C3@outlook.com>
2020-10-16 12:01:12 +0800

    ch8 8.1

---
+++ python_work/ch8/8.1.1 greeter.py
@@ -0,0 +1,8 @@
+
+def greet_user2(username):  # 这里的参数是形参
+    """ Show simple greetings to someone. """
+
+    print(f'Hello, {username.title()}!')    # 函数的功能
+
+greet_user2('cassie')   # 语句中传入的参数叫实参
+
