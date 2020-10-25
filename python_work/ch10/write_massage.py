09d376a28951b25237e3b6970d1a44e29aa710d9
WeiS49 <swh_1C3@outlook.com>
2020-10-25 18:22:38 +0800

    ch10 10.2

---
+++ python_work/ch10/write_massage.py
@@ -0,0 +1,6 @@
+
+filename = "python_work/ch10/programming.txt" # 需要提供相对路径名
+with open(filename, 'a') as file_object:
+    file_object.write("I also love finding meaning in large datasets.\n")
+    file_object.write("I love creating apps that can run in a browser.\n")
+
